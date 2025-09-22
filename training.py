038cdeeaa94c690b98191fad95ee5c1b730aa126
dperkins3pi <dperkins3pi@gmail.com>
2025-09-22 12:57:02 -0400

    Set up venv

---
+++ training.py
@@ -0,0 +1,8 @@
+import torch
+import cv2
+import numpy as np
+import pandas as pd
+import mediapipe as mp
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score
